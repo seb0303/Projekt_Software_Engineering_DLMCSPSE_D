9010e9aaff6c3728ebe8876a9989440e002fbade
seb0303 <sebastian.uhler@ymail.com>
2025-04-13 11:43:28 +0200

    Added doxygen for main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,26 @@
 #include "include/welcomescreenDialog.h"
 #include "include/init.h"
 
+/**
+ * @file main.cpp
+ * @brief Einstiegspunkt der PennyWise-Anwendung
+ *
+ * Diese Datei enthält die `main()`-Funktion und ist verantwortlich für den Start der Anwendung.
+ * Hier wird die QApplication initialisiert, das WelcomeScreen-Fenster erstellt und angezeigt.
+ * Von hier aus verzweigt der Benutzer entweder zur Demo-Datenbank, zur Erstellung einer neuen Datenbank
+ * oder zum Öffnen einer bestehenden.
+ *
+ * Hauptverantwortlichkeiten:
+ * - Initialisierung des Qt-Eventloops
+ * - Laden des WelcomeScreens
+ * - Verzweigung zu MainWindow nach Benutzerauswahl
+ * - Initialisierung des Logs
+ *
+ * @author Sebastian Uhler - 92203600; Kurs: Projekt Software Engineering DLMCSPSE_D
+ * @date 2025
+ */
+
+
 QFile logFile;
 
 void logHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
